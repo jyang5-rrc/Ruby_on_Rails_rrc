101ad4e2d0468db8ade83dc0c996ba61c76d4b79
jyang5 <jyang5@academic.rrc.ca>
2024-02-03 16:19:04 -0600

    delete files

--- challenges/U3/modles/customer.rb
+++
@@ -1,9 +0,0 @@
-class Customer < ActiveRecord::Base
-  # Based on the class name "Customer"
-  # AR will guess that the db table name is "customers"
-
-  belongs_to :province # There is a province_id FK in the customers table
-
-  validates :name, :email, :city, presence: true
-  validates :email, uniqueness: true
-end
