466c3904b3929ae29b7dd2e1cf4082c1603c4262
jyang5 <jyang5@academic.rrc.ca>
2024-02-03 16:16:46 -0600

    ar_read challenge

--- challenges/U3/challenge_read.rb
+++ challenges/U3/challenge_read.rb
@@ -1,5 +1,30 @@
+=begin
+challenge: U3 - ActiveRecord
+creater: @Jiajia Yang
+create data: 2024-02-04
+update data: 2024-02-04
+description: Active record Coding Challenge
+=end
 require_relative 'ar.rb'
 
-number_of_products = Product.count
+#Use the Product class to find (any) product from the database
+product = Product.first
 
+#Inspect the Product object you have retrieved
+puts product.inspect
+
+#record all the columns that exist in the products table
+puts Product.column_names
+#based on the columns we can see products table has an association with the categories table, because it has a category_id column.
+
+#Total number of products
+number_of_products = Product.count
 puts "There are #{number_of_products} in the products table."
+
+#The names of all products above $10 with names that begin with the letter C
+products_above_10_with_C = Product.where("price > 10").where("name LIKE 'C%'").pluck(:name)
+puts "The names of all products above $10 with names that begin with the letter C are: \n#{products_above_10_with_C.join("\n")}"
+
+#Total number of products with a low stock quantity. (Low is defined as less than 5 in stock.)
+products_with_low_stock_number = Product.where("stock_quantity < 5").count
+puts "There are #{products_with_low_stock_number} products with a low stock quantity."
